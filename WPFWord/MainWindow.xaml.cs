e25cad6509a670448cdb2fc83a54d1e4b745c9ce
Ataki_pw11@itstep.edu.az <Ataki_pw11@itstep.edu.az>
2023-03-03 01:30:19 +0400

    selectall done

--- WPFWord/MainWindow.xaml.cs
+++ WPFWord/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +31,46 @@ namespace WPFWord
         {
             if(e.Key == Key.Enter)
             {
-                contentTxtb.Text +='\n';
+                //contentTxtb.SelectAll();
+            }
+        }
+
+        private void copyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataObjectCopyingEventArgs eventArgs=contentTxtb.DataContext as DataObjectCopyingEventArgs;
+        }
+
+        private void selectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            contentTxtb.SelectAll();
+        }
+
+        private void openBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Filter = "All Files(*.*)|*.*|Text Files(*.txt)| *.txt";
+            openDialog.FilterIndex = 2;
+
+
+            if (openDialog.ShowDialog() == true)
+            {
+                using (var sr = File.OpenText(openDialog.FileName))
+                {
+                    contentTxtb.Text = sr.ReadToEnd();
+                }
+            }
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            if (save.ShowDialog() == true)
+            {
+                using (var sw = new StreamWriter(save.FileName))
+                {
+                    sw.Write(contentTxtb.Text);
+                    contentTxtb.Text = String.Empty;
+                }
             }
         }
     }
